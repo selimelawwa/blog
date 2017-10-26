4d2b0e167068a1753e1790cfcf25d9fe30830ee9
selimelawwa <selimalawwa335@gmail.com>
2017-10-26 13:56:12 +0200

    comment get request for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'welcome/index'  #might comment it?
+  #get 'welcome/index'  #might comment it?
  
 resources :articles do
   resources :comments
