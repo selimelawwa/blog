f8fbd5d1a02e6dcd9088dec8ebbbf103c83f89b3
selimelawwa <selimalawwa335@gmail.com>
2017-11-01 16:47:04 +0200

    migrations for user, some styiling and validation for articles and user

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+    has_many :articles
+    has_many :comments, dependent: :destroy
+    before_save {self.email = email.downcase}
+    validates :username, presence: true, uniqueness: {case_sensitive: false},
+    length: { minimum: 5, maximum: 25 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: {case_sensitive: false},
+    length: { maximum: 105 }, format: {with: VALID_EMAIL_REGEX }
+
+end
